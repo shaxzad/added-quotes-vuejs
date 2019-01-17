ea2f0d1cd48c93102ace893fedd9394068bbe471
ejoo <ejaz@droidox.com>
2019-01-17 23:41:05 +0500

    working on the adding quotes project

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"></appHeader>
+    <app-new-quote @quoteAdded="newQuote"> </app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a Quote to delete it!
+        </div>
+      </div> 
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
+export default {
+  data: function() {
+    return {
+        quotes: [
+          'I love programing'
+        ],
+        maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes ) {
+        return alert("Please delete Quotes first!")
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: { 
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
